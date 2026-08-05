108662ff4f4113a4301372f5a9ba2fb6f209a5ce
David Ragnar Nelson <drnelson6@gmail.com>
2026-08-05 11:31:08 -0400

    Initial draft of models

---
+++ pubs/models.py
@@ -0,0 +1,151 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import datetime
+from django_edit_away.models import TiptapField
+
+
+ANNOTATORS = {
+        "AM": "Alex Mazzaferro",
+        "JA": "Jeff Appelhans",
+        "EMJ": "Unknown EMJ",
+        "AV": "Unknown AV",
+        "JF": "Julie Fisher",
+        'CP': 'Unknown CP',
+        'SB': 'Unknown SB',
+        'YS': 'Unknown YS',
+        'GS': 'Unknown GS',
+        'CF': 'Unknown CF',
+    }
+
+
+def validate_year(value):
+    """Ensure that years entered are plausible"""
+    current_year = datetime.now().year
+    if value < 1700 or value > current_year:
+        raise ValidationError(
+            ("%(value)s does not match the expected date scope for this project"),
+            params={"value": value},
+        )
+
+
+def verify_latlon(value):
+    """Proxy validation for latitude and longitude that checks whether
+    value is a decimal value between -180 and 180."""
+    if not -180 <= value <= 180:
+        raise ValidationError(
+            "Latitude or longitude must be between -180 and 180 degrees."
+        )
+
+
+class Subject(models.Model):
+    AUTHORITY_CHOICES = {
+        'APS': 'American Philosophical Society',
+        'LOC': 'Library of Congress',
+    }
+
+    # Question: should this save the URL or just the ID?
+    uri = models.CharField(max_length=20, blank=True, null=True)
+    heading = models.CharField(max_length=200)
+    authority_source = models.CharField(choices=AUTHORITY_CHOICES, max_length=3)
+
+    # for complex subjects
+    coponents = models.ManyToManyField(
+        'self', blank=True, symmetrical=False, related_name='component_of'
+    )
+
+    class Meta:
+        ordering = ['heading']
+
+
+class Member(models.Model):
+    bib_number = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    suffix = models.CharField(max_length=50, blank=True)
+    # this should actually validate dates for the project, but oh well
+    election_date = models.PositiveIntegerField(validators=[validate_year])
+    office = models.CharField(max_length=100, blank=True)
+    bio = TiptapField()
+    bio_note = models.TextField(blank=True)
+    # Once DB is set up, set this to default to a the generic image to reduce storage redundancy
+    image = models.ImageField(upload_to='images', default='default.jpg')
+    image_alt_text = models.CharField(max_length=255)
+    # is this field public facing?
+    note = models.TextField(blank=True)
+    # need to remediate to allow multiples
+    created_by = models.CharField(max_length=3, choices=ANNOTATORS, blank=True)
+    drupal_nid = models.PositiveIntegerField(blank=True, null=True, editable=False)
+    authority_record = models.ForeignKey(Subject, on_delete=models.PROTECT)
+
+
+class Creator(models.Model):
+    label = models.CharField(max_length=200)
+    subject = models.ForeignKey(Subject, blank=True, null=True, on_delete=models.PROTECT)
+    # TODO: Implement relator more robustly - probably requires loc-authorities patch
+    relator = models.CharField(max_length=20)
+
+
+class Publication(models.Model):
+    RECORD_SOURCE_CHOICES = {
+        "AAS": "American Antiquarian Society",
+        "APS": "American Philosophical Society",
+        "BNF": "Bibliothèque nationale de France",
+        "CORN": "Cornell Uniersity Library",
+        "ESTC": "English Short Title Catalogue",
+        "FOUNDERS": "Founders Online",
+        "HARV": "Harvard Library",
+        "HSP": "Historical Society of Pennsylvania",
+        "HUNT": "The Huntington",
+        "JCB": "John Carter Brown Library",
+        "LCP": "Library Company of Philadelphia",
+        "LOC": "Library of Congress",
+        "NBY": "The Newberry",
+        "NLM": "National Library of Medicine",
+        "NYHS": "The New York Historical",
+        "NYPL": "New York Public Library",
+        "PENN": "University of Pennsylvania Libraries",
+        "PU": "Princeton University Library",
+        "WC": "Worldcat",
+        "YALE": "Yale Library",
+    }
+
+    identifier = models.CharField(max_length=50)
+    editions_note = models.TextField(blank=True)
+    holding_note = TiptapField()
+    members = models.ManyToManyField(Member)
+    creators = models.ManyToManyField(Creator)
+    title = models.TextField()
+    year_published = models.PositiveIntegerField(validators=[validate_year])
+    publication = models.CharField(max_length=255)
+    subjects = models.ManyToManyField(Subject)
+    record_source = models.CharField(choices=RECORD_SOURCE_CHOICES, max_length=10)
+    # revise this field
+    references = models.CharField(max_length=255, blank=True)
+    aps_record_link = models.URLField(null=True, blank=True)
+    record_permalink = models.URLField()
+    drupal_nid = models.PositiveIntegerField(blank=True, null=True, editable=False)
+    # TODO: This needs to accept multiple values somehow
+    annotator = models.CharField(choices=ANNOTATORS, max_length=3)
+
+
+class Edition(models.Model):
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    number_of_editions = models.PositiveIntegerField()
+    year = models.PositiveIntegerField(validators=[validate_year])
+    place_name = models.CharField(max_length=50)
+    latitude = models.DecimalField(
+        max_digits=8,
+        decimal_places=5,
+        blank=True,
+        null=True,
+        validators=[verify_latlon],
+    )
+    longitude = models.DecimalField(
+        max_digits=8,
+        decimal_places=5,
+        blank=True,
+        null=True,
+        validators=[verify_latlon],
+    )
+
+    # TODO: Add method for querying external service to retrieve coordinates
---
+++ pubs/tests.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from django.test import TestCase
+import pytest
+from django.core.exceptions import ValidationError
+
+from .models import validate_year, verify_latlon
+
+
+class TestValidators(TestCase):
+    def test_year_is_valid(self):
+        """Test whether validate_year raises correct exceptions"""
+        current_year = datetime.now().year
+        # valid
+        validate_year(1743)
+        # valid
+        validate_year(current_year)
+
+        with pytest.raises(ValidationError) as excinfo:
+            validate_year(600)
+        assert "600 does not match the expected date scope for this project" in str(
+            excinfo.value
+        )
+
+        with pytest.raises(ValidationError) as excinfo:
+            next_year = current_year + 1
+            validate_year(next_year)
+        assert (
+            f"{next_year} does not match the expected date scope for this project"
+            in str(excinfo.value)
+        )
+
+    def test_lat_lon_is_valid(self):
+        """Test whether lat/long validation errors are raised"""
+
+        # valid
+        verify_latlon(52.520008)
+        # valid
+        verify_latlon(-75.1652)
+
+        with pytest.raises(ValidationError) as excinfo:
+            verify_latlon(-181.35214)
+        assert "Latitude or longitude must be between -180 and 180 degrees." in str(
+            excinfo.value
+        )
+
+        with pytest.raises(ValidationError) as excinfo:
+            verify_latlon(181.35214)
+        assert "Latitude or longitude must be between -180 and 180 degrees." in str(
+            excinfo.value
+        )
